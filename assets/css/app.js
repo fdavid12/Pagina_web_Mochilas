c17be1ce166fc8359171bf49982b57c93acfecc4
David <davimp.245@gmail.com>
2026-02-11 10:43:16 -0500

    Actializacion

---
+++ assets/css/app.js
@@ -0,0 +1,10 @@
+const images = document.querySelectorAll('img[data-lightbox]');
+
+if (images); {
+//load the lightbox 
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+
+
+
+}
